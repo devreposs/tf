498c1831224f4337312817aa6f4bcf6937478602
devreposs <151166350+devreposs@users.noreply.github.com>
2023-12-13 08:42:52 +0530

    Delete Terraform Modules/AIC-modules directory

--- Terraform Modules/AIC-modules/terraform-aws-ms-sql/examples/sql-server-2019-enterprise/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  sql_version             = "2019"
-  sql_edition             = "enterprise"
-  sql_backups_s3_bucket   = "backup-${var.name}"
-  create_s3_backup_bucket = true
-  instance_type           = "m5.xlarge"
-  env                     = "dev"
-  maintenance_window      = "TUE_00"
-}
